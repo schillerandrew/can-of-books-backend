7b2c72a33c18a71e0ccb6cc6419673f82a43d1bd
schillerandrew <schiller.andrew@gmail.com>
2022-04-25 15:05:30 -0700

    added collaboration plan to README, did initial building of server code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//// REQUIRES
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -7,8 +8,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+//// add mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL);
+
+//// define PORT to validate env is working
 const PORT = process.env.PORT || 3001;
 
+//// ROUTES
 app.get('/test', (request, response) => {
 
   response.send('test request received')
